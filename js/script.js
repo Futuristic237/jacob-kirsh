5aad6fbc2a30cd39189d7f30018486212c9d4daa
Futuristic237 <108652241+Futuristic237@users.noreply.github.com>
2022-07-25 15:30:04 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+    // NavBar Hide On Click Outside Start
+
+    $(document).ready(function () { 
+        $(document).click(function () {
+           // if($(".navbar-collapse").hasClass("in")){
+             $('.navbar-collapse').collapse('hide');
+           // }
+        });
+      });
+
+    // NavBar Hide On Click Outside End
